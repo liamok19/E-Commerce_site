bf2c52b6654230080f056ca221aab8f8bd197c06
Liam Ok <liamokane19@gmail.com>
2022-03-01 14:05:27 +1100

    got rid of the comment out for the primary key. ProductTag requires Product_id as a primary key

--- models/Product.js
+++ models/Product.js
@@ -13,7 +13,7 @@ Product.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      // primaryKey: true,
+      primaryKey: true,
       autoIncrement: true
     },
     product_name: {
